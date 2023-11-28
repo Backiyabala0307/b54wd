3caa9ec82bb0b7def7c7fb01386b41acad7b09e6
Kavyabala0307 <kavyabala3799@gmail.com>
2023-11-28 20:44:53 +0530

    day4

---
+++ Roadmap/day4/functions.js
@@ -0,0 +1,27 @@
+// function sayHello() {
+//     console.log("hello world");
+// }
+
+// for (let i = 10; i <= 19; i++){
+//     sayHello();
+// }
+
+// //task
+// function product(m,n) {
+//     console.log(m*n);
+// }
+// product(5, 6);
+
+// //find square root
+// function sqrt(m) {
+//     console.log(m**0.5);
+// }
+//  sqrt(5);
+
+
+
+// function sqrt(m) {
+//     return m**0.5;
+// }
+//let result=sqrt(5);
+//console.log(result);
